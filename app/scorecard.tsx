9e54f6115bb0a9a4f8e05655ba7b9beb667788a6
Johnny Rosas <JJRosas@student.fullsail.edu>
2025-02-21 20:12:06 -0500

    changed name to match the rest of the file names

--- app/scorecard.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function ScorecardScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text> Scorecard Screen! </Text>
-    </View>
-  );
-}
