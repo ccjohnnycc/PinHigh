1c89640b48cff4824a6d8684be6071ec284e114c
Johnny Rosas <JJRosas@student.fullsail.edu>
2025-02-27 00:24:16 -0500

    removed default compass. in the process of creating my own

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,15 @@
-import { Tabs } from "expo-router";
 import { Stack } from "expo-router";
-import Icon from "react-native-vector-icons/Ionicons";
-
 
 export default function RootLayout() {
-    return (
-      <Stack>
-      <Stack.Screen name="index" options={{ title: "PinHigh" }} />
-      <Stack.Screen name="CourseSearchScreen" options={{ title: "Search Course" }} />
-      <Stack.Screen name="map" options={{ title: "Map" }} />
-      <Stack.Screen name="scorecard" options={{ title: "Scorecard" }} />
-      <Stack.Screen name="SignInScreen" options={{ title: "Sign In" }} />
-      <Stack.Screen name="TeeSelectionScreen" options={{ title: 'Select Tee' }} />
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="CourseSearchScreen" />
+      <Stack.Screen name="map" />
+      <Stack.Screen name="scorecard" />
+      <Stack.Screen name="SignInScreen" />
+      <Stack.Screen name="TeeSelectionScreen" />
 
     </Stack>
-    );
-  }
+  );
+}
