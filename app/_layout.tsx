672bb20afda5b85183a4f6ae81f5f42233cc8a17
Johnny Rosas <JJRosas@student.fullsail.edu>
2025-02-28 22:40:40 -0500

    removed the course search file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,6 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
-      <Stack.Screen name="CourseSearchScreen" />
       <Stack.Screen name="map" />
       <Stack.Screen name="scorecard" />
       <Stack.Screen name="SignInScreen" />
